8863bdea457c80205823bb8c0d9bcdea7a684f6a
Ahmed Nasr <S21510777EG@std.aou.edu.eg>
2023-12-04 21:35:43 +0200

    install Reactjs and Tailwindcss

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='font-semibold text-xl text-red-500'>hh</h1>
+    </>
+  )
+}
+
+export default App
