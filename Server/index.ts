acd865c469c97b4f4a9d74b8ef348aa2f9e25112
Ahmed Nasr <S21510777EG@std.aou.edu.eg>
2023-12-07 19:26:00 +0200

    Create and Connect To Database

--- Server/index.ts
+++ Server/index.ts
@@ -1,7 +1,16 @@
 const express =require('express')
+const mongoose= require('mongoose')
+const dotenv=require('dotenv')
+dotenv.config()
+
+
+mongoose.connect(process.env.DATABASE).then(()=>{
+    console.log("DataBase Connected")
+});
+
 
 const app= express();
 
-app.listen(5000,()=>{
-    console.log(" server connected..")
+app.listen(process.env.PORT,()=>{
+    console.log("Server Connected.")
 })
