8c66140d2b42dbf4d56921471582ab4ce77729d2
Ahmed Nasr <S21510777EG@std.aou.edu.eg>
2023-12-09 02:09:20 +0200

    Complete Sign in Functionality

--- Server/Controllers/AuthControllers.ts
+++ Server/Controllers/AuthControllers.ts
@@ -20,9 +20,9 @@ export const signin=async(req:Request,res:Response,next:any)=>{
     const {email,password} =req.body
     try{
         const validUser=await User.findOne({email})
-        if(!validUser) return next(errorHandler("User not found !"))
+        if(!validUser) return res.json(errorHandler("User not found !"))
         const validPass=bcrypt.compareSync(password,validUser.password)
-        if (!validPass) return next(errorHandler("Wrong password"))
+        // if (!validPass) return next(errorHandler("Wrong password").message)
         const token=jwt.sign({_id:validUser._id}, "123rfca5dx5w") 
         const {password:pass,...rest} = validUser._doc
         res.cookie('access Token',token,{httpOnly:true}).status(200).json(rest)
--- vite-project/src/Pages/SignIn.tsx
+++ vite-project/src/Pages/SignIn.tsx
@@ -1,13 +1,54 @@
-import { Link } from "react-router-dom"
+import { ChangeEvent, EventHandler, FormEvent, useState } from "react"
+import toast, { Toaster } from "react-hot-toast"
+import { Link,useNavigate } from "react-router-dom"
 
 interface Iprops{
 
 }
 const SignIn=(props:Iprops)=>{
+    const [formdata,Setformdata]=useState({})
+    const [error,Seterror]=useState('')
+    const navigate=useNavigate()
+    const handleSubmit=async(e:FormEvent<HTMLFormElement>)=>{
+        e.preventDefault();
+        try{
+            const res=await fetch('/api/auth/signin',{
+                method:"POST" ,
+                headers:{"Content-Type":"Application/json"}
+                ,body:JSON.stringify(formdata)
+            })
+            .then(res=>res.json())
+            .then(data=>toast.success("Welcome Back : "+data.username))
+            setTimeout(()=>navigate('/'),2000);
+        }   
+        catch(error){
+            console.log(error)
+        }
+    }
+    const handleInputs=(event:ChangeEvent<HTMLInputElement>)=>{
+        Setformdata({
+            ...formdata,
+            [event.target.id]:event.target.value
+        })
+    }
     return(
         <>
-            
-        </>
+        <div className="flex flex-col  my-7  gap-4 items-center">
+            <h1 className="font-semibold text-2xl ">Sign Up</h1>
+            <form onSubmit={handleSubmit} action="" className="w-full gap-4 px-5 flex flex-col" method="post">
+                <input id="email" type="text" placeholder="email"  className="focus:outline-none focus:border-gray-400 border h-[50px] rounded-lg p-1  w-full" onChange={handleInputs}/>
+                <input id="password" type="password" placeholder="password"  className="focus:outline-none focus:border-gray-400 border h-[50px] rounded-lg p-1  w-full" onChange={handleInputs}/>
+                <button className="bg-gray-700 h-[50px] rounded-xl text-white hover:bg-gray-400 disabled:opacity-75">SIGN IN</button>
+                {/* <button className="bg-red-700 h-[50px] rounded-xl text-white hover:bg-red-400 disabled:opacity-75">CONTINUE WITH GOOGLE</button> */}
+            </form>
+            <div>
+                <p className="text-lg">Donot Have an account?
+                <Link to='/signup'><span className="font-semibold px-1 text-blue-600 hover:border-b-2 hover:border-blue-600">Sign up</span></Link>
+                </p>
+            </div>
+        </div>
+        <Toaster position="bottom-center"/>
+    </>
     )
 }
 export default SignIn
